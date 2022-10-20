0ccb0330dea127615db9ba15d1a99b8a0fa6293c
CudraMohamed <ncudramohamed@gmail.com>
2022-10-20 16:30:35 +0300

    added wallet functionality

--- api/models.py
+++ api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+#deposit API to deposit funds to any accoun
+def deposit(self, amount):
+       if amount <= 0:
+           message =  "Invalid amount"
+           status = 403
+       else:
+           self.account_balance += amount
+           self.save()
+           message = f"You have deposited {amount}, your new balance is {self.account_balance}"
+           status = 200
+       return message, status
+
+
+#transfer funds to another account to another.
+
+def transfer(self, destination, amount):
+       if amount <= 0:
+           message =  "Invalid amount"
+           status = 403
+      
+       elif amount < self.account_balance:
+           message =  "Insufficient balance"
+           status = 403
+      
+       else:
+           self.account_balance -= amount
+           self.save()
+           destination.deposit(amount)
+          
+           message = f"You have transfered {amount}, your new balance is {self.account_balance}"
+           status = 200
+       return message, status
